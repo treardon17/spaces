c9558f731230a454e3acada911b18fc61b7e0402
Tyler Reardon <tdr.reardon@gmail.com>
2018-02-19 13:05:15 -0800

    Restarting

--- Pods/Target Support Files/CCNLaunchAtLoginItem/CCNLaunchAtLoginItem-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CCNLaunchAtLoginItem : NSObject
-@end
-@implementation PodsDummy_CCNLaunchAtLoginItem
-@end
--- Pods/Target Support Files/Pods-Spaces/Pods-Spaces-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Spaces : NSObject
-@end
-@implementation PodsDummy_Pods_Spaces
-@end
