cf40e75660beb07a81f9ad4e40d5c539165598c7
Tyler Reardon <tdr.reardon@gmail.com>
2018-03-04 20:20:35 -0800

    Code cleanup

--- Spaces/AppDelegate.swift
+++ Spaces/AppDelegate.swift
@@ -17,55 +17,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var window:TROverlayWindow? = nil;
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        _ = TRWindowManager.sharedInstance
-        // Start of Window Manager
-//        let runningApps = TRWindowManager.sharedInstance.runningApps
-//        for (_, app) in runningApps{
-//            app.onMove = { element in
-//                print("app moved: \(app.title)")
-//                print(element)
-//                let window = element as? SIWindow
-//                print(window)
-//                print(window?.frame())
-//                if NSEvent.mouseLocation().x == 0{
-//                    if let window = window, let screen = window.screen(){
-//                        let width = screen.frame.width / 2
-//                        let height = screen.frame.height
-//                        window.setFrame(CGRect(x: 0, y: 0, width: width, height: height))
-//                    }
-//                }
-//            }
-//            
-//            app.onApplicationActivated = { element in
-//                print("app activated: \(app.title)")
-//            }
-//            
-//            app.onCreate = { element in
-//                print("app created: \(app.title)")
-//            }
-//            
-//            app.onFocusChanged = { element in
-//                print("focus change: \(app.title)")
-//            }
-//            
-//            app.onMiniaturized = { element in
-//                print("app miniaturized: \(app.title)")
-//            }
-//            
-//            app.onDeminiaturized = { element in
-//                print("app deminiaturized: \(app.title)")
-//            }
-//        }
-        
-//        // Show a mockup window
-//        let frame = NSScreen.main()?.frame
-//        if let frame = frame{
-//            self.window = TROverlayWindow(contentRect: NSRect(x: 0, y: 0, width: 0, height: frame.size.height))
-//            self.window?.show()
-//            
-//            let rect = NSRect(x: 0, y: 0, width: frame.size.width / 2, height: frame.size.height)
-//            self.window?.setDimensions(rect, animated: true)
-//        }
+        _ = TRWindowManager.shared
+        _ = TRDataManager.shared
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
