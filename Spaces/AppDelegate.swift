b067e8334b8fff0cbebcbb349f6ac2b604385a92
Tyler Reardon <tdr.reardon@gmail.com>
2018-03-14 22:31:38 -0700

    Add menubar icon

--- Spaces/AppDelegate.swift
+++ Spaces/AppDelegate.swift
@@ -13,7 +13,7 @@ import SwiftyDropbox
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    let statusItem:NSStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength);
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
     var darkModeOn:Bool = true;
     var window:TROverlayWindow? = nil;
 
@@ -22,6 +22,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         _ = TRConfigManager.shared
         TRSyncManager.shared.setupSync()
         
+        if let button = statusItem.button {
+            let image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+            let imageWidth = image?.size.width
+            let imageHeight = image?.size.height
+            let imageProp = imageHeight! / imageWidth!
+            let newImageWidth:CGFloat = 20
+            let newImageHeight = newImageWidth * imageProp
+            button.image = image
+            button.image?.size = NSSize(width: newImageWidth, height: newImageHeight)
+            button.action = #selector(printQuote(_:))
+        }
+    }
+    
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) — \(quoteAuthor)")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
