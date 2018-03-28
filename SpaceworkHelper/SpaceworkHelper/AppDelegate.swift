129866c39f11cc4abfec7c2fd4328d1648247147
Tyler Reardon <tdr.reardon@gmail.com>
2018-03-28 08:27:50 -0700

    Debugging for helper

--- SpaceworkHelper/SpaceworkHelper/AppDelegate.swift
+++ SpaceworkHelper/SpaceworkHelper/AppDelegate.swift
@@ -10,10 +10,20 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func write(text: String, to fileNamed: String, folder: String = "SavedFiles") {
+        guard let path = NSSearchPathForDirectoriesInDomains(.downloadsDirectory, .userDomainMask, true).first else { return }
+        guard let writePath = NSURL(fileURLWithPath: path).appendingPathComponent(folder) else { return }
+        try? FileManager.default.createDirectory(atPath: writePath.path, withIntermediateDirectories: true)
+        let file = writePath.appendingPathComponent(fileNamed + ".txt")
+        try? text.write(to: file, atomically: false, encoding: String.Encoding.utf8)
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        if NSRunningApplication.runningApplications(withBundleIdentifier: "com.rockrabbit.Spacework").isEmpty {
-            NSWorkspace.shared.launchApplication("Spacework")
-        }
+        
+        let directory = FileManager.default.currentDirectoryPath
+        let string = "directory: \(directory))\nbundle: \(Bundle.main.bundleIdentifier!)\nsuccess: \(NSWorkspace.shared.launchApplication("Spacework"))"
+        self.write(text: string, to: "log", folder: "spacework_logs")
         NSApp.terminate(nil)
     }
 }
