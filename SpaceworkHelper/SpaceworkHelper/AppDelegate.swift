77337cc2b69c482021962221426001213519b801
Tyler Reardon <tdr.reardon@gmail.com>
2018-03-28 19:21:39 -0700

    Launch on startup works

--- SpaceworkHelper/SpaceworkHelper/AppDelegate.swift
+++ SpaceworkHelper/SpaceworkHelper/AppDelegate.swift
@@ -10,20 +10,19 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    func write(text: String, to fileNamed: String, folder: String = "SavedFiles") {
-        guard let path = NSSearchPathForDirectoriesInDomains(.downloadsDirectory, .userDomainMask, true).first else { return }
-        guard let writePath = NSURL(fileURLWithPath: path).appendingPathComponent(folder) else { return }
-        try? FileManager.default.createDirectory(atPath: writePath.path, withIntermediateDirectories: true)
-        let file = writePath.appendingPathComponent(fileNamed + ".txt")
-        try? text.write(to: file, atomically: false, encoding: String.Encoding.utf8)
-    }
+//    func write(text: String, to fileNamed: String, folder: String = "SavedFiles") {
+//        guard let path = NSSearchPathForDirectoriesInDomains(.downloadsDirectory, .userDomainMask, true).first else { return }
+//        guard let writePath = NSURL(fileURLWithPath: path).appendingPathComponent(folder) else { return }
+//        try? FileManager.default.createDirectory(atPath: writePath.path, withIntermediateDirectories: true)
+//        let file = writePath.appendingPathComponent(fileNamed + ".txt")
+//        try? text.write(to: file, atomically: false, encoding: String.Encoding.utf8)
+//    }
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        
-        let directory = FileManager.default.currentDirectoryPath
-        let string = "directory: \(directory))\nbundle: \(Bundle.main.bundleIdentifier!)\nsuccess: \(NSWorkspace.shared.launchApplication("Spacework"))"
-        self.write(text: string, to: "log", folder: "spacework_logs")
+//        let directory = FileManager.default.currentDirectoryPath
+//        let string = "directory: \(directory))\nbundle: \(Bundle.main.bundleIdentifier!)\nsuccess: \(NSWorkspace.shared.launchApplication("Spacework"))"
+//        self.write(text: string, to: "log", folder: "spacework_logs")
+        NSWorkspace.shared.launchApplication("Spacework")
         NSApp.terminate(nil)
     }
 }
